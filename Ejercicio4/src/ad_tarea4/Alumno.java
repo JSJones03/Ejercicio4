e514ac8b9d0ef1e3d8f71febf4f2e356d8e128d8
jesus <jesus@DESKTOP-9KDG1QM>
2024-10-20 22:35:08 +0200

    Proyecto terminado

--- Ejercicio4/src/ad_tarea4/Alumno.java
+++ Ejercicio4/src/ad_tarea4/Alumno.java
@@ -2,79 +2,181 @@ package ad_tarea4;
 
 import java.time.LocalDate;
 
+/**
+ * En esta clase Alumno, definimos los atributos del alumno Contiene información
+ * como nombre, apellido, nia, fecha de nacimiento, curso, ciclo, grupo y
+ * genero.
+ * 
+ * @author Jesús
+ * 
+ */
 public class Alumno {
 	private int nia;
 	private String nombre, apellido, ciclo, curso, grupo;
 	private LocalDate fechaNacimiento;
 	private char genero;
 
+	/**
+	 * Método constructor
+	 */
 	public Alumno() {
 	}
 
+	/**
+	 * Devuelve el nia del alumno
+	 * 
+	 * @return nia
+	 */
 	public int getNia() {
 		return nia;
 	}
 
+	/**
+	 * Establece el nia del alumno.
+	 * 
+	 * @param nia del alumno.
+	 * 
+	 */
 	public void setNia(int nia) {
 		this.nia = nia;
 	}
 
+	/**
+	 * Devuelve el nombre del alumno
+	 * 
+	 * @return nombre
+	 */
 	public String getNombre() {
 		return nombre;
 	}
 
+	/**
+	 * Establece el nombre del alumno.
+	 * 
+	 * @param nombre del alumno.
+	 * 
+	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-
+	/**
+	 * Devuelve el apellido del alumno
+	 * 
+	 * @return apellido
+	 */
 	public String getApellido() {
 		return apellido;
 	}
 
+	/**
+	 * Establece el apellido del alumno.
+	 * 
+	 * @param apellido del alumno.
+	 * 
+	 */
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
 	}
-
+	/**
+	 * Devuelve el ciclo del alumno
+	 * 
+	 * @return ciclo
+	 */
 	public String getCiclo() {
 		return ciclo;
 	}
 
+	/**
+	 * Establece el ciclo del alumno.
+	 * 
+	 * @param ciclo del alumno.
+	 * 
+	 */
 	public void setCiclo(String ciclo) {
 		this.ciclo = ciclo;
 	}
-
+	/**
+	 * Devuelve el curso del alumno
+	 * 
+	 * @return curso
+	 */
 	public String getCurso() {
 		return curso;
 	}
 
+	/**
+	 * Establece el curso del alumno.
+	 * 
+	 * @param curso del alumno.
+	 * 
+	 */
 	public void setCurso(String curso) {
 		this.curso = curso;
 	}
 
+	/**
+	 * Devuelve el grupo del alumno
+	 * 
+	 * @return grupo
+	 */
 	public String getGrupo() {
 		return grupo;
 	}
 
+	/**
+	 * Establece el grupo del alumno.
+	 * 
+	 * @param grupo del alumno.
+	 * 
+	 */
 	public void setGrupo(String grupo) {
 		this.grupo = grupo;
 	}
 
+	/**
+	 * Devuelve la fecha de nacimiento del alumno
+	 * 
+	 * @return fechaNacimiento del alumno
+	 */
 	public LocalDate getFechaNacimiento() {
 		return fechaNacimiento;
 	}
 
+	/**
+	 * Establece la fecha de nacimiento del alumno.
+	 * 
+	 * @param fechaNacimiento del alumno.
+	 * 
+	 */
 	public void setFechaNacimiento(LocalDate fechaNacimiento) {
 		this.fechaNacimiento = fechaNacimiento;
 	}
 
+	/**
+	 * Devuelve el genero del alumno
+	 * 
+	 * @return genero
+	 */
 	public char getGenero() {
 		return genero;
 	}
 
+	/**
+	 * Establece el genero del alumno.
+	 * 
+	 * @param genero del alumno.
+	 * 
+	 */
 	public void setGenero(char genero) {
 		this.genero = genero;
 	}
 
+	/**
+	 * Anhiade a una cadena los atributos del alumno
+	 * 
+	 * @return Cadena con los detalles del alumno con su nia, nombre, apellido,
+	 *         ciclo, curso, grupo, fecha de nacimiento, y su genero.
+	 */
 	@Override
 	public String toString() {
 		return "Alumno [nia=" + nia + ", nombre=" + nombre + ", apellido=" + apellido + ", ciclo=" + ciclo + ", curso="
--- Ejercicio4/src/ad_tarea4/Test.java
+++ Ejercicio4/src/ad_tarea4/Test.java
@@ -1,107 +1,155 @@
 package ad_tarea4;
 
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+/**
+ * Esta clase permite introducir datos de alumnos y guardarlos en un archivo
+ * binario. Luego, estos datos pueden ser leídos y mostrados.
+ * <p>
+ * El programa solicita datos como nombre, apellido, nia, fecha de nacimiento,
+ * ciclo, curso, grupo y género. Valida las entradas y luego almacena los datos
+ * en un archivo binario.
+ * </p>
+ * 
+ * @author Jesús
+ */
 public class Test {
-
+	/**
+	 * Metodo principal donde se realiza la introducción de ficheros y alumnos.
+	 * 
+	 * @param args Argumentos de la línea de comandos
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		ArrayList<Alumno> listaAlumnos = new ArrayList<>();
+		System.out.println("Introduzca la ruta donde quiere guardar a los alumnos ");
+		String ruta = sc.nextLine();
+		System.out.println("Escriba el nombre del fichero:");
+		String nombre = sc.nextLine();
+		nombre = nombre + ".dat";
+		File ficheroBin = new File(ruta + "\\" + nombre);
+
+		FileOutputStream os;
+		DataOutputStream dos = null;
 
 		System.out.println("Introduce 5 alumnos");
 		Alumno alum;
-		Scanner sc = new Scanner(System.in);
-		ArrayList<Alumno> listaAlumnos = new ArrayList<>();
-		for (int i = 0; i < 5; i++) {
-			int dia = 0, mes = 0, anhio = 0;
-			String vCurso, vCiclo, grupo;
-			char genero;
+		try {
+			os = new FileOutputStream(ficheroBin, true);
+			dos = new DataOutputStream(os);
 
-			System.out.println("Alumno " + (i + 1));
-			alum = new Alumno();
-			System.out.println("Introduce el nombre:");
-			alum.setNombre(sc.next());
-			System.out.println("Introduce el Apellido:");
-			alum.setApellido(sc.next());
-			try {
-				do {
-					System.out.println("Introduce el NIE(9 digitos):");
-					alum.setNia(sc.nextInt());
-				} while (Integer.toString(alum.getNia()).length() != 9);
-			} catch (InputMismatchException e) {
-				System.out.println("El NIE solo puede tener números");
-				alum.setNia(0);
-			}
+			for (int i = 0; i < 5; i++) {
 
-			do {
 				try {
-					sc.nextLine();
-					System.out.println("Introduce la fecha de nacimiento");
-					System.out.println("Introduce dia (1-30):");
-					dia = sc.nextInt();
 
-					System.out.println("Introduce mes(1-12):");
-					mes = sc.nextInt();
+					int dia = 0, mes = 0, anhio = 0;
+					String vCurso, vCiclo, grupo;
+					char genero;
 
-					System.out.println("Introduce anho(1924-2025):");
-					anhio = sc.nextInt();
-				} catch (InputMismatchException e) {
-					System.out.println("Solo se pueden escribir numeros enteros");
+					System.out.println("Alumno " + (i + 1));
+					alum = new Alumno();
+					System.out.println("Introduce el nombre:");
+					alum.setNombre(sc.next());
+					dos.writeUTF(alum.getNombre());
+					System.out.println("Introduce el Apellido:");
+					alum.setApellido(sc.next());
+					dos.writeUTF(alum.getApellido());
+					do {
+						sc.nextLine();
+						try {
+							System.out.println("Introduce el NIE(9 digitos):");
+							alum.setNia(sc.nextInt());
 
+						} catch (InputMismatchException e) {
+							System.out.println("El NIE solo puede tener números");
+						}
+					} while (Integer.toString(alum.getNia()).length() != 9);
+					dos.writeInt(alum.getNia());
+					do {
+						try {
+							sc.nextLine();
+							System.out.println("Introduce la fecha de nacimiento");
+							System.out.println("Introduce dia (1-30):");
+							dia = sc.nextInt();
+
+							System.out.println("Introduce mes(1-12):");
+							mes = sc.nextInt();
+
+							System.out.println("Introduce anho(1924-2025):");
+							anhio = sc.nextInt();
+						} catch (InputMismatchException e) {
+							System.out.println("Solo se pueden escribir numeros enteros");
+						}
+
+					} while ((dia < 1 || dia > 31) || (mes < 1 || mes > 12) || (anhio < 1924 || anhio > 2024));
+
+					alum.setFechaNacimiento(LocalDate.of(anhio, mes, dia));
+					dos.writeUTF(alum.getFechaNacimiento().toString());
+
+					vCiclo = sc.nextLine();
+					do {
+						System.out.println("Introduce el ciclo:");
+						System.out.println("DAM\tDAW\tASIR");
+						vCiclo = sc.nextLine();
+					} while (!(vCiclo.toUpperCase().equalsIgnoreCase("DAM")
+							|| vCiclo.toUpperCase().equalsIgnoreCase("DAW")
+							|| vCiclo.toUpperCase().equalsIgnoreCase("ASIR")));
+
+					alum.setCiclo(vCiclo);
+					dos.writeUTF(alum.getCiclo());
+					do {
+						System.out.println("Introduce el curso:");
+						System.out.println("Primero\t Segundo");
+						vCurso = sc.nextLine();
+					} while (!(vCurso.equalsIgnoreCase("Primero") || vCurso.equalsIgnoreCase("Segundo")));
+					alum.setCurso(vCurso);
+					dos.writeUTF(alum.getCurso());
+
+					do {
+						System.out.println("Introduce el grupo");
+						System.out.println("1. A\t2. B\t3. C");
+						grupo = sc.nextLine();
+					} while (!(grupo.equalsIgnoreCase("A") || grupo.equalsIgnoreCase("B")
+							|| grupo.equalsIgnoreCase("C")));
+					alum.setGrupo(grupo);
+					dos.writeUTF(alum.getGrupo());
+					do {
+						System.out.println("Introduce el género: ");
+						System.out.println("1. H\t2. M");
+						genero = sc.next().toUpperCase().charAt(0);
+					} while (genero != 'H' && genero != 'M');
+					alum.setGenero(genero);
+					dos.writeChar(alum.getGenero());
+					listaAlumnos.add(alum);
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
 				}
-			} while ((dia < 1 || dia > 31) || (mes < 1 || mes > 12) || (anhio < 1924 || anhio > 2024));
-
-			alum.setFechaNacimiento(LocalDate.of(anhio, mes, dia));
-
-			vCiclo = sc.nextLine();
-			do {
-				System.out.println("Introduce el ciclo:");
-				System.out.println("DAM\tDAW\tASIR");
-				vCiclo = sc.nextLine();
-			} while (!(vCiclo.toUpperCase().equalsIgnoreCase("DAM") || vCiclo.toUpperCase().equalsIgnoreCase("DAW")
-					|| vCiclo.toUpperCase().equalsIgnoreCase("ASIR")));
-
-			alum.setCiclo(vCiclo);
-
-			do {
-				System.out.println("Introduce el curso:");
-				System.out.println("Primero\t Segundo");
-				vCurso = sc.nextLine();
-			} while (!(vCurso.equalsIgnoreCase("Primero") || vCurso.equalsIgnoreCase("Segundo")));
-			alum.setCurso(vCurso);
-
-			do {
-				System.out.println("Introduce el grupo");
-				System.out.println("1. A\t2. B\t3. C");
-				grupo = sc.nextLine();
-			} while (!(grupo.equalsIgnoreCase("A") || grupo.equalsIgnoreCase("B") || grupo.equalsIgnoreCase("C")));
-			alum.setGrupo(grupo);
-
-			do {
-				System.out.println("Introduce el género: ");
-				System.out.println("1. H\t2. M");
-				genero = sc.next().toUpperCase().charAt(0);
-			} while (genero != 'H' && genero != 'M');
-			alum.setGenero(genero);
-
-			alum.setGenero(genero);
-
-			if (alum.getNia() == 0) {
-				System.out.println("Nie error de introducción\nIntroduzca los datos de nuevo");
-				i = i - 1;
-			} else {
-				listaAlumnos.add(alum);
 			}
-		}
 
-		for (Alumno alumno : listaAlumnos) {
-			System.out.println(alumno);
-		}
+			for (Alumno alumno : listaAlumnos) {
+				System.out.println(alumno);
+			}
 
-		sc.close();
+			dos.close();
+			sc.close();
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IOException e) {
+
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
-
 }
