a5b9f1b86156bbfe33cfe421220a0adff446ada8
jesus <jesus@DESKTOP-7EO9RK8>
2024-10-20 12:55:48 +0200

    Primer comentario

---
+++ Ejercicio4/src/ad_tarea4/Alumno.java
@@ -0,0 +1,84 @@
+package ad_tarea4;
+
+import java.time.LocalDate;
+
+public class Alumno {
+	private int nia;
+	private String nombre, apellido, ciclo, curso, grupo;
+	private LocalDate fechaNacimiento;
+	private char genero;
+
+	public Alumno() {
+	}
+
+	public int getNia() {
+		return nia;
+	}
+
+	public void setNia(int nia) {
+		this.nia = nia;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getCiclo() {
+		return ciclo;
+	}
+
+	public void setCiclo(String ciclo) {
+		this.ciclo = ciclo;
+	}
+
+	public String getCurso() {
+		return curso;
+	}
+
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+
+	public String getGrupo() {
+		return grupo;
+	}
+
+	public void setGrupo(String grupo) {
+		this.grupo = grupo;
+	}
+
+	public LocalDate getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(LocalDate fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	public char getGenero() {
+		return genero;
+	}
+
+	public void setGenero(char genero) {
+		this.genero = genero;
+	}
+
+	@Override
+	public String toString() {
+		return "Alumno [nia=" + nia + ", nombre=" + nombre + ", apellido=" + apellido + ", ciclo=" + ciclo + ", curso="
+				+ curso + ", grupo=" + grupo + ", fechaNacimiento=" + fechaNacimiento + ", genero=" + genero + "]";
+	}
+
+}
